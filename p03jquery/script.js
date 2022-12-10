c5f53c850f432bc117783cd17948f0a48027d023
azizchairul <112891370+azizchairul@users.noreply.github.com>
2022-12-10 14:03:41 +0700

    Delete p03jquery directory

--- p03jquery/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-    $("td").click(function (event) {
-        alert("Sekarang kamu berada di " + $(this).html() + "!");
-      });
-      $("th").click(function (event) {
-        alert("Sekarang kamu berada di " + $(this).html() + "!");
-      });
-    });
